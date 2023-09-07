16118d3957e6938acfc6894b66c6d9d368379c9f
yui880 <kimyuna521@gmail.com>
2023-09-07 20:06:49 +0900

    refactor : problem4 알파벳 변경 함수 수정, 알파벳 대소문자 확인 함수 수정

--- src/problem4.js
+++ src/problem4.js
@@ -1,36 +1,29 @@
+const convertToReversedLetter = (type, letter) => {
+  const first = type === "upper" ? "A" : "a";
+  const last = type === "upper" ? "Z" : "z";
+
+  const CODE_SUM = first.charCodeAt(0) + last.charCodeAt(0);
+  const letterCode = letter.charCodeAt(0);
+
+  return String.fromCharCode(CODE_SUM - letterCode);
+};
+
+const checkCaseType = (letter) => {
+  if (/[A-Z]/.test(letter)) return "upper";
+  else if (/[a-z]/.test(letter)) return "lower";
+  else return "-1";
+};
+
 function problem4(word) {
   let answer = "";
   for (let i = 0; i < word.length; i++) {
-    const type = checkType(word[i]);
+    const type = checkCaseType(word[i]);
 
     if (type === "-1") answer += word[i];
-    else answer += makeReverse(word[i], type);
+    if (type === "upper") answer += convertToReversedLetter("upper", word[i]);
+    if (type === "lower") answer += convertToReversedLetter("lower", word[i]);
   }
   return answer;
 }
-
-const makeReverse = (ch, type) => {
-  const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const reversedUpper = upper.split("").reverse().join("");
-  const lower = upper.toLowerCase();
-  const reversedLower = lower.split("").reverse().join("");
-
-  if (type === "upper") {
-    return changeAlphabet(upper, reversedUpper, ch);
-  } else {
-    return changeAlphabet(lower, reversedLower, ch);
-  }
-};
-
-const changeAlphabet = (before, after, ch) => {
-  const index = before.indexOf(ch);
-  return after[index];
-};
-
-const checkType = (ch) => {
-  if (ch >= "a" && ch <= "z") return "lower";
-  if (ch >= "A" && ch <= "Z") return "upper";
-  else return "-1";
-};
 
 module.exports = problem4;
